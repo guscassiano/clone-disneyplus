e1604b4e3b98d4befa7cba6d259e3d36a2757025
Gustavo Cassiano <gucpinto26@gmail.com>
2023-02-14 02:03:51 -0300

    Corrigindo FAQ

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
 const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -11,13 +12,20 @@ function styles() {
         .pipe(gulp.dest('./dist/css'))
 }
 
+function scripts () {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
 function images() {
     return gulp.src('./src/images/**')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, scripts, images);
 exports.watch = function (){
     gulp.watch('./src/styles/*.scss', gulp.series(styles))
+    gulp.watch('./src/scripts/*.js', gulp.series(scripts))
 }
